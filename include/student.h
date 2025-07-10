ac3a663abb794ea4bc3b67754dc11273650b0349
VictorVikaki <vikakikimani@gmail.com>
2025-07-10 16:00:02 +0300

    feat: add project skeleton with main, students, and utils files

---
+++ include/student.h
@@ -0,0 +1,10 @@
+#ifndef STUDENT_H
+#define STUDENT_H
+
+void addStudent();
+void viewStudents();
+void searchStudent();
+void editStudent();
+void deleteStudent();
+
+#endif // STUDENT_H
---
+++ src/student.cpp
@@ -0,0 +1,31 @@
+// logic for students operations
+
+#include <iostream>
+#include "../include/student.h"
+
+using namespace std;
+
+void addStudent() {
+    // TODO: Implement logic to add a student and write to file
+    cout << "[addStudent] function called\n";
+}
+
+void viewStudents() {
+    // TODO: Implement logic to read and display all students
+    cout << "[viewStudents] function called\n";
+}
+
+void searchStudent() {
+    // TODO: Implement logic to search by name or ID
+    cout << "[searchStudent] function called\n";
+}
+
+void editStudent() {
+    // TODO: Implement logic to modify an existing student
+    cout << "[editStudent] function called\n";
+}
+
+void deleteStudent() {
+    // TODO: Implement logic to delete a student from records
+    cout << "[deleteStudent] function called\n";
+}
