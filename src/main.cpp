1536018225d03e9862f1c11a49eb98baf38ea90a
VictorVikaki <vikakikimani@gmail.com>
2025-07-17 00:00:44 +0300

    feat: add the deleteStudent() function in the menu

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,3 @@
-// entry point for the program
-
 #include <iostream>
 #include "../include/student.h"
 #include "../include/utils.h"
@@ -7,42 +5,35 @@
 using namespace std;
 
 int main() {
-    vector<Student> studentList;
+    int choice;
 
-    addStudent(studentList);
+    do {
+        cout << "\n--- Student Management System ---" << endl;
+        cout << "1. Add Student" << endl;
+        cout << "2. Delete Student" << endl;
+        cout << "3. Edit Student" << endl;
+        cout << "4. Exit" << endl;
+        cout << "Enter your choice: ";
+        cin >> choice;
+        cin.ignore();
 
-    cout << "\n--- Current Students in System ---" << endl;
-
-    if (studentList.empty()) {
-        cout << "No students in the system yet." << endl;
-    } else {
-        for (const auto& student : studentList) {
-            student.displayStudent();
+        switch (choice) {
+            case 1:
+                addStudent();
+                break;
+            case 2:
+                deleteStudent();
+                break;
+            // case 3:
+            //     editStudent();
+            //     break;
+            case 4:
+                cout << "Exiting the system." << endl;
+                break;
+            default:
+                cout << "Invalid choice, please try again." << endl;
         }
-    }
-    // int choice;
-
-    // while (true) {
-    //     cout << "\n=== Student Management System ===\n";
-    //     cout << "1. Add Student\n";
-    //     cout << "2. View All Students\n";
-    //     cout << "3. Search Student\n";
-    //     cout << "4. Edit Student\n";
-    //     cout << "5. Delete Student\n";
-    //     cout << "0. Exit\n";
-    //     cout << "Enter your choice: ";
-    //     cin >> choice;
-
-    //     switch (choice) {
-    //         case 1: addStudent(); break;
-    //         case 2: viewStudents(); break;
-    //         case 3: searchStudent(); break;
-    //         case 4: editStudent(); break;
-    //         case 5: deleteStudent(); break;
-    //         case 0: cout << "Goodbye!\n"; return 0;
-    //         default: cout << "Invalid choice. Try again.\n";
-    //     }
-    // }
+    } while (choice != 4);
 
     return 0;
 }
